bd6076e6404ebab12f9189af6b57811e56a169a6
Johannes Stein <johannesstein@freeze-dev.com>
2016-11-29 13:47:11 +0100

    Updates

--- webpack-server-bundles/dist/demos/ggj2016/images/index.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var req = require.context('./', false, /\.(png|jpg|gif|js)$/);
-
-module.exports = req.keys().reduce(function(obj, name) {
-  var moduleName = path.basename(name).split(path.extname(name))[0];
-
-  obj[moduleName] = req(name);
-  return obj;
-}, {});
